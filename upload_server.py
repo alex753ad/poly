ea7281775a9f849710a12e73704d8304295de84a
alex753ad <alex753ad@gmail.com>
2026-04-11 21:35:07 +0300

    add upload server

---
+++ upload_server.py
@@ -0,0 +1,16 @@
+import http.server, os
+
+class Handler(http.server.BaseHTTPRequestHandler):
+    def do_PUT(self):
+        length = int(self.headers['Content-Length'])
+        data = self.rfile.read(length)
+        with open('/data/polymarket_v2.db', 'wb') as f:
+            f.write(data)
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'OK')
+    def log_message(self, *a): pass
+
+port = int(os.environ.get('PORT', 8080))
+print(f'Upload server on port {port}')
+http.server.HTTPServer(('0.0.0.0', port), Handler).serve_forever()
