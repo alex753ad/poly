68e6059c0f88dd76769aeb2f0033aeba809f894a
alex753ad <alex753ad@gmail.com>
2026-04-20 14:16:44 +0300

    add download support

--- upload_server.py
+++ upload_server.py
@@ -9,6 +9,15 @@ class Handler(http.server.BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b'OK')
+    def do_GET(self):
+        with open('/data/polymarket_v2.db', 'rb') as f:
+            data = f.read()
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/octet-stream')
+        self.send_header('Content-Disposition', 'attachment; filename="polymarket_v2.db"')
+        self.send_header('Content-Length', str(len(data)))
+        self.end_headers()
+        self.wfile.write(data)
     def log_message(self, *a): pass
 
 port = int(os.environ.get('PORT', 8080))
